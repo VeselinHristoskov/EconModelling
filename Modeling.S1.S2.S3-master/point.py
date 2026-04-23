1b418e7c5206000681792f88bb2db01a6a0f2e92
VeselinHristoskov <topvesko@gmail.com>
2026-04-24 00:09:14 +0200

    Add files via upload

---
+++ Modeling.S1.S2.S3-master/point.py
@@ -0,0 +1,59 @@
+class Point:
+    """
+    Simple class to represent a point in 2D space.
+    """
+    def __init__(self, x, y):
+        """
+        Constructor for Point class.
+        :param x: x coordinate of point.
+        :param y: y coordinate of point.
+        """
+        self.x = x # x is a class attribute
+        self.y = y
+    def __str__(self):
+        """
+        String representation of Point class.
+        :return: string representation of Point class.
+        """
+        return f"P<{self.x},{self.y}>"
+    def __repr__(self):
+        return self.__str__()
+    def distance_origin(self):
+        """
+        Calculate the distance between point and origin.
+        :return: float, distance between point and origin.
+        """
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def distance_to(self, point):
+        """
+        Calculate the distance between current point and another point.
+        :param point: the other point to calculate the distance to.
+        :return: float, distance between current point and another point.
+        """
+        return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) **  0.5
+    def __lt__(self, other):
+        """
+        Returns True is self is less than the other point.
+        :param other: the other point to compare against
+        :return: True or False
+        """
+        return self.distance_origin() < other.distance_origin()
+
+if __name__ == "__main__": # protect the code from import runs!!
+    p1 = Point(1, 2)
+    p2 = Point(3, 4)
+    print(p1.x, p1.y)
+    print(p2.x, p2.y)
+    print(p1, p2) # P<1,2>
+    print(f"{p2} distance to origin is {p2.distance_origin()}")
+    p3 = Point(12, 5)
+    print(f"{p3} distance to origin is {p3.distance_origin()}")
+    p1 = Point(6, 10)
+    p2 = Point(6, 15)
+    print(f"distance between {p1} and {p2} is {p1.distance_to(p2)}")
+    p4 = Point(1, 1)
+    points = [p1, p2, p3, p4, Point(15, 6)] # list of points
+    print(points)
+    points.sort()
+    print(points)
---
+++ poker.gameV2-master/poker.py
@@ -0,0 +1,76 @@
+from deck import Deck
+class PokerHand:
+    """
+    Class representing a poker hand
+    We will work to rank the hand and determine trips, full house, 4 of a kind, etc
+    """
+    def __init__(self):
+        deck = Deck()
+        deck.shuffle()
+        _cards = []
+        for i in range(5):
+            _cards.append(deck.deal())
+        self._cards = _cards
+
+    def __str__(self):
+        return str(self._cards)
+
+
+    @property
+    def is_flush(self):
+        first_card_suit= self._cards[0].suit
+        for i in range(1,5):
+            if first_card_suit != self._cards[i].suit:
+                return False
+        return True
+
+    @property
+    def matches(self):
+        # need to compare each card against all the others and count matches
+        matches = 0
+        for card1 in self._cards:
+            for card2 in self._cards:
+                if card1 == card2:
+                    continue
+                if card1.rank == card2.rank:
+                    matches += 1
+        return matches
+
+    @property
+    def is_fullhouse(self):
+        return self.matches == 8
+
+    @property
+    def is_4kind(self):
+        return self.matches == 12
+
+    @property
+    def is_straight(self):
+        if self.matches != 0:
+            return False
+        self._cards.sort()
+        ranks = self._cards[0].RANKS
+        if ranks.index(self._cards[0].rank) + 4 == ranks.index(self._cards[4].rank):
+            return True
+        return False
+
+hand = PokerHand()
+print(hand)
+hand._cards.sort()
+print(hand)
+
+iterations = 0
+hits = 0
+while True:
+    hand = PokerHand()
+    iterations += 1
+    if hand.is_4kind:
+            print(hand)
+            print("Done it in", iterations)
+            hits += 1
+    if hits == 100:
+            prob = hits / iterations * 100  # gives in percentage
+            print(f"probability of a flush is {prob}%")
+            break
+
+
